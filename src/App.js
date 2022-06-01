bfd24bd480688a5e458045f219fcdbf7153291cc
Casey Harding <casey.r.harding@gmail.com>
2022-06-01 15:52:45 -0400

    Initial dummy data in place.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import './App.css';
 
+const initialUser = {
+  gender: "male",
+  name: {
+    first: "Casey",
+    last: "Harding"
+  },
+  location: {
+    state: "NY"
+  },
+  email: "C@C.com",
+  login: {
+    uuid: "blahblahblah",
+    username: "CRHarding"
+  },
+  picture: {
+    large: "https://downloadwallpaper.org/wp-content/uploads/2016/05/thor-wallpaper3.jpg"
+  }
+}
+
 function App() {
   return (
     <div className="App">
