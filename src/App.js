bd6c6418a450a4cccce6bbf62f9d6a5f2f5ecd30
Casey Harding <casey.r.harding@gmail.com>
2022-06-01 16:29:20 -0400

    Initial API call in place.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import initialUser from "./data";
 
 import Profile from "./Components/Profile";
 
 function App() {
   const [user, setUser] = useState(initialUser);
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://randomuser.me/api/?results=5")
+      .then(res => {
+        console.log(res.data.results);
+      }).catch(err => console.error(err));
+  }, [])
+  
   return (
     <div className="App flex-column">
       <h1>CASEYBOOK</h1>
